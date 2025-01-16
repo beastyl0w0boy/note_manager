40c9191c799b1e949354578d5f83cbd3b1eefc85
beastyl0w0boy <g4nieff.m@yandex.ru>
2025-01-16 15:38:30 +0300

    date_changer.py

---
+++ date_changer.py
@@ -0,0 +1,8 @@
+created_date = '17.01.2025'
+issue_date = '17.02.2025'
+created_date2 = '17.январь.2025'
+issue_date2 = '17.февраль.2025'
+print(created_date[0:5])
+print(issue_date[0:5])
+print(created_date2[0:9])
+print(issue_date2[0:10])
